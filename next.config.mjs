4c3566f82604a68932e54129a39396d594a81845
rustanSalvador-ba <feliperustan@gmail.com>
2025-09-22 16:15:45 -0300

    refatoração

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true, // evita falhas de build por causa de warnings do ESLint
-  },
-  typescript: {
-    ignoreBuildErrors: true, // evita falhas de build por causa de erros de TypeScript
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Evita que módulos que usam 'fs' ou 'path' quebrem no cliente
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
